3d15f5d4b33f3343622390d9ca6cdccfdf8685a5
Nicolas Peña <yeison.nicolas112@outlook.com>
2024-04-07 20:21:26 -0500

    commit de prueba

---
+++ EasyParkingAPI-master/EasyParking.Api/Program.cs
@@ -0,0 +1,48 @@
+using EasyParking.Api.Data;
+using EasyParking.Api.Services.Contracts;
+using EasyParking.Api.Services.Implementation;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EasyParkingContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("EasyParkingConnection"));
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<UserService, UserServiceImplement>();
+
+
+
+var app = builder.Build();
+
+using(var scope= app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<EasyParkingContext>();
+    dataContext.Database.Migrate();
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
